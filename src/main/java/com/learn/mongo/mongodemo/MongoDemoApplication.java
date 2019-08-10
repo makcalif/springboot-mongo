12599b746415f87023b90a98c21089b59f261658
Mumtaz Khan <makcalif@yahoo.com>
2019-08-10 10:53:05 -0500

    removed unnecessary clientDiscover annotation and gradle deps that are not needed for this service to be discoverable

--- src/main/java/com/learn/mongo/mongodemo/MongoDemoApplication.java
+++ src/main/java/com/learn/mongo/mongodemo/MongoDemoApplication.java
@@ -4,12 +4,12 @@ import com.learn.mongo.mongodemo.entities.Person;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import java.util.UUID;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 
 public class MongoDemoApplication implements CommandLineRunner {
 
